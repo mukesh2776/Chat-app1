ff27f5cec91fa3e686c1c777b7d83256b404ed70
mukesh2776 <mukeshsr943@gmail.com>
2025-07-05 18:24:08 +0530

    Update login.js

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -10,7 +10,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res =await axios.post("http://localhost:5000/api/login", { email, password });
+      const res =await axios.post("https://chat-app1-backend.onrender.com/api/login", { email, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('username', res.data.user.username);
       router.push('/chat');
